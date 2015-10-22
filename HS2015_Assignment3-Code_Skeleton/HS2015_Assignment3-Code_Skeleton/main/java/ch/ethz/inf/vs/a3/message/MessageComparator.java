650a78c7e18f2aee63fdd7c78a08831289cce132
zinggand <zinggand@ethz.ch>
2015-10-22 11:17:47 +0200

    some updated things ... :)

---
+++ HS2015_Assignment3-Code_Skeleton/HS2015_Assignment3-Code_Skeleton/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
@@ -0,0 +1,18 @@
+package ch.ethz.inf.vs.a3.message;
+
+import java.util.Comparator;
+import ch.ethz.inf.vs.a3.solution.message.Message;
+
+/**
+ * Message comparator class. Use with PriorityQueue.
+ */
+public class MessageComparator implements Comparator<Message> {
+
+    @Override
+    public int compare(Message lhs, Message rhs) {
+        // Write your code here
+
+        return 0;
+    }
+
+}
